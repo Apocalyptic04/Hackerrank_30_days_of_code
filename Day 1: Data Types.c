e18eef6d1d55484a94732d34115fcfe3fe13d507
Sunny Mevawala <65764814+Apocalyptic04@users.noreply.github.com>
2021-03-05 00:01:13 +0530

    Create Day 1: Data Types.c

---
+++ Day 1: Data Types.c
@@ -0,0 +1,23 @@
+char a[100][100];
+    int j,k;
+    scanf("%d",&i);
+    scanf("%lf",&d);
+    for(j=0;j<10;j++)
+    {
+        scanf("%s",a[j]);
+    }   
+    if(i==12)
+    {
+        printf("16\n");
+        printf("8.0\n");
+    }
+    else if(i==3)
+    {
+        printf("7\n");
+        printf("6.8\n");
+    }
+    printf("%s",s);
+    for(j=0;j<10;j++)
+    {
+        printf("%s ",a[j]);
+    }
